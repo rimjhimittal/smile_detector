2969777d0eb644050b08cee6ee9b8177d07b04ff
rimjhimittal <rmittal_be21@thapar.edu>
2022-10-06 16:31:47 +0530

    Face Detected

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import cv2
 cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 while True:
     _, frame = cap.read()
-    cv2.imshow('Lets Click', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for x, y, w, h in face:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
+    cv2.imshow('Say Cheese', frame)
     if cv2.waitKey(10) == ord('q'):
         break
